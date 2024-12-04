e211109b6fb5dc78f8b4430760eeb6e195f6f426
Mohammed Samy <mom118580@gmail.com>
2024-12-04 15:15:36 -0800

    display onboarding views

---
+++ lib/models/onboarding_model.dart
@@ -0,0 +1,13 @@
+class OnboardingModel {
+  final String image;
+  final String title_1;
+  final String title_2;
+  final String subtitle;
+
+  OnboardingModel({
+    required this.image,
+    required this.title_1,
+    required this.title_2,
+    required this.subtitle,
+  });
+}
--- lib/views/onboarding_view.dart
+++ lib/views/onboarding_view.dart
@@ -1,75 +1,147 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_mobile_app/models/onboarding_model.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingView extends StatelessWidget {
-  const OnboardingView({super.key});
+  OnboardingView({
+    super.key,
+  });
+  PageController pageController = PageController();
+  // final OnboardingModel onboardingModelc;
+
+  List<OnboardingModel> onboarding = [
+    OnboardingModel(
+      image: 'assets/images/onbording_1.png',
+      title_1: 'Welcome to Fresh Fruits',
+      title_2: 'Grocery application',
+      subtitle:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+    ),
+    OnboardingModel(
+      image: 'assets/images/onbording_2.png',
+      title_1: 'We provide best quality Fruits to your family',
+      title_2: '',
+      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed',
+    ),
+    OnboardingModel(
+      image: 'assets/images/onbording_3.png',
+      title_1: 'Fast and responsibily delivery by our courir ',
+      title_2: '',
+      subtitle:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
+    // OnboardingModel onboardingModel;
+
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            pageController.previousPage(
+              duration: Duration(milliseconds: 500),
+              curve: Curves.fastLinearToSlowEaseIn,
+            );
+          },
+          icon: Icon(
+            Icons.arrow_back,
+          ),
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(28.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // image
-            Image.asset('assets/images/onbording_1.png'),
-            // title
-            Text(
-              'Welcome to Fresh Fruits',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Text(
-              'Grocery application',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            // subtitle
-            Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ',
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-            SizedBox(
-              height: 24,
-            ),
+            Expanded(
+              child: PageView.builder(
+                controller: pageController,
+                itemCount: onboarding.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      // image
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: ClipRRect(
+                          child: Image.asset(
+                            onboarding[index].image,
+                            fit: BoxFit.cover,
+                            height: 180,
+                            width: 303,
+                          ),
+                        ),
+                      ),
+                      // title_1
+                      Text(
+                        onboarding[index].title_1,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24,
+                      ),
+                      // titile_2
+                      Text(
+                        onboarding[index].title_2,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      // subtitle
+                      Text(
+                        onboarding[index].subtitle,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24,
+                      ),
 
-            // dots
-            SmoothPageIndicator(
-              controller: PageController(),
-              count: 3,
-              effect: ExpandingDotsEffect(
-                activeDotColor: Color(0xff12B76A),
-                dotColor: Color(0xffF2F2F2),
-                dotHeight: 6,
-                dotWidth: 8,
-                spacing: 5,
+                      // dots
+                      SmoothPageIndicator(
+                        controller: pageController,
+                        count: onboarding.length,
+                        effect: ExpandingDotsEffect(
+                          activeDotColor: Color(0xff12B76A),
+                          dotColor: Color(0xffF2F2F2),
+                          dotHeight: 6,
+                          dotWidth: 8,
+                          spacing: 5,
 
-                // strokeWidth: 8,
+                          // strokeWidth: 8,
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 41,
+                      ),
+                    ],
+                  );
+                },
               ),
-            ),
-
-            SizedBox(
-              height: 41,
             ),
 
             // button
             TextButton(
-              onPressed: () {},
+              onPressed: () {
+                pageController.nextPage(
+                  duration: Duration(milliseconds: 500),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                );
+              },
               style: TextButton.styleFrom(
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(32),
